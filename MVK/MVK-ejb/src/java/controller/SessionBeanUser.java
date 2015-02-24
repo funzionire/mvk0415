bdcd501fb2c5b9c817b427e894ea6ef74441ac48
Harmankardon88 <softwharmankardon@web.de>
2015-02-24 16:19:03 +0100

    SessionBeanUser initiiert

--- MVK/MVK-ejb/src/java/controller/SessionBeanUser.java
+++ MVK/MVK-ejb/src/java/controller/SessionBeanUser.java
@@ -5,14 +5,50 @@
  */
 package controller;
 
+import java.util.List;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.FlushModeType;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import model.User;
 
 /**
  *
  * @author Felix
  */
-@Stateless
+@Stateless(name = "SessionBeanUser")
 public class SessionBeanUser implements SessionBeanUserLocal {
+
+    @PersistenceContext
+    private EntityManager em;
+    
+    @Override
+    public User createPerson(String name, String email, String password) {
+        em.setFlushMode(FlushModeType.AUTO);
+        User user = new User(name, email, password);
+        em.persist(user);
+        user = em.merge(user);
+        em.flush();
+        return user;
+        
+    }
+
+    @Override
+    public User login(String email, String password) {
+        if (email == null || password == null || email.isEmpty() || password.isEmpty())
+        {
+            return null;
+        }
+        TypedQuery<User> query = em.createNamedQuery("User.findByLoginPasswort", User.class)
+            .setParameter("email", email)
+            .setParameter("password", password);
+        if (query.getResultList().isEmpty())
+        {
+            return null;
+        }
+        return query.getSingleResult();
+    }
 
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
